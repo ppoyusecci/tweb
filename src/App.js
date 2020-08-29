3bd6bc688e326d752573ebf9dc82a7ee4a6727a4
Tom_desktop <tydhtoom@gmail.com>
2020-08-29 12:40:46 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a personal website about Tom
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ppoyusecci"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Github Page
         </a>
       </header>
     </div>
